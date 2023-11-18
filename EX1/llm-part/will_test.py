62a9a424f0f4ad7df6739bcd58bd48a8a1aae2b1
will amminger <willamminger@gmail.com>
2023-11-18 15:31:50 +0100

    created quanitzed models

--- EX1/llm-part/will_test.py
+++ EX1/llm-part/will_test.py
@@ -1,64 +1,3 @@
-# https://huggingface.co/docs/transformers/main_classes/quantization
-
-from awq import AutoAWQForCausalLM
-from transformers import AutoTokenizer
-
-model_path = "mhemetfaik/flan-t5-large-copy"
-quant_path = '4test'
-
-#quant_config = {"zero_point": True, "q_group_size": 128, "w_bit": 4, "version":"GEMM"}
-quant_config = {"w_bit": 4}
-
-# Load model
-model = AutoAWQForCausalLM.from_pretrained(model_path)
-tokenizer = AutoTokenizer.from_pretrained(model_path, trust_remote_code=True)
-
-# Quantize
-model.quantize(tokenizer, quant_config=quant_config)
-
-
-
-from transformers import AwqConfig, AutoConfig
-from huggingface_hub import HfApi
-
-# modify the config file so that it is compatible with transformers integration
-quantization_config = AwqConfig(
-	bits = quant_config["w_bit"],
-).to_dict()
-
-# the pretrained transformers model is stored in the model attribute + we need to pass a dict
-model.model.config.quantization_config = quantization_config
-# a second solution would be to use Autoconfig and push to hub (what we do at llm-awq)
-
-
-# save model weights
-model.save_quantized(quant_path)
-tokenizer.save_pretrained(quant_path)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -73,3 +12,5 @@ from datasets import load_dataset
 dataset = pd.DataFrame(load_dataset("lambada")['validation']['text'], columns=['text'])
 
 dataset[['input', 'target']] = dataset['text'].str.rsplit(pat=' ', n=1, expand=True)
+
+#%%
