a272b864a3b588ea479a04448cd6c3a087397be8
will amminger <willamminger@gmail.com>
2023-11-20 13:58:47 +0100

    re-quantize models without use_exllama

--- EX1/llm-part/will_test.py
+++ EX1/llm-part/will_test.py
@@ -13,4 +13,21 @@ dataset = pd.DataFrame(load_dataset("lambada")['validation']['text'], columns=['
 
 dataset[['input', 'target']] = dataset['text'].str.rsplit(pat=' ', n=1, expand=True)
 
-#%%
+
+
+
+import torch
+test = torch.hub.load('facebook/opt-125m' 'custom', path='EX1/llm-part/model2', force_reload=True, model='opt-125m')
+
+
+model = torch.load('model2/config.json')
+
+import json
+
+obj = json.load(open('model2/config.json', 'r'))
+
+
+
+from transformers import AutoModelForCausalLM
+test = AutoModelForCausalLM.from_pretrained('model2')
+
