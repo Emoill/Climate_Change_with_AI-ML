e43c01296e22513c646f46fd2fe0c67fac31bab2
will amminger <willamminger@gmail.com>
2023-11-20 15:26:19 +0100

    got to RuntimeError: "LayerNormKernelImpl" not implemented for 'Half'

--- EX1/llm-part/will_test.py
+++ EX1/llm-part/will_test.py
@@ -14,7 +14,7 @@ dataset = pd.DataFrame(load_dataset("lambada")['validation']['text'], columns=['
 dataset[['input', 'target']] = dataset['text'].str.rsplit(pat=' ', n=1, expand=True)
 
 
-
+'''
 
 import torch
 test = torch.hub.load('facebook/opt-125m' 'custom', path='EX1/llm-part/model2', force_reload=True, model='opt-125m')
@@ -27,7 +27,14 @@ import json
 obj = json.load(open('model2/config.json', 'r'))
 
 
+'''
 
-from transformers import AutoModelForCausalLM
-test = AutoModelForCausalLM.from_pretrained('model2')
 
+from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
+
+pipe = pipeline(
+	task = "text-generation",
+	model = AutoModelForCausalLM.from_pretrained('model2'), # loaded from directory
+	tokenizer = AutoTokenizer.from_pretrained('facebook/opt-125m') # re-use from original model
+)
+pipe.predict('today i want to go to the')[0]['generated_text']
