2543441734782b13aafc1106aaa7ac2ae058fe85
alexman98 <41584365+alexman98@users.noreply.github.com>
2024-01-06 22:34:38 +0100

    Updated LSTM for grid search and train test split f.

--- EX2/utils.py
+++ EX2/utils.py
@@ -4,20 +4,24 @@ import pandas as pd
 seed = 42
 
 def get_test_table(scaled=False):
-	if scaled:
-		file = 'data_temp/gauge24scaled.csv'
-	else:
-		file = 'data_temp/gauge24.csv'
+    if scaled:
+        file = 'data_temp/gauge24scaled.csv'
+    else:
+        file = 'data_temp/gauge24.csv'
 
+    df = pd.read_csv(file)
 
+    df = df.sort_values('date_column')
 
-	df = pd.read_csv(file)
+    response = 'prec'
+    X = df.drop([response], axis=1)
+    y = df[response]
 
-	response = 'prec'
+    split_point = int(len(df) * 0.7)
+    
+    X_train = X.iloc[:split_point]
+    y_train = y.iloc[:split_point]
+    X_test = X.iloc[split_point:]
+    y_test = y.iloc[split_point:]
 
-	X = df.drop([response], axis=1)
-	y = df[response]
-
-	X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=seed)
-
-	return X_train, X_test, y_train, y_test
+    return X_train, X_test, y_train, y_test
