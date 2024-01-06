e3ec1ebcbcf3070683e7104c37d5dad422f06f9d
will amminger <willamminger@gmail.com>
2024-01-06 18:36:24 +0100

    created util function to easily load train-test split

--- EX2/utils.py
+++ EX2/utils.py
@@ -1,1 +1,23 @@
+from sklearn.model_selection import train_test_split
+import pandas as pd
+
 seed = 42
+
+def get_test_table(scaled=False):
+	if scaled:
+		file = 'data_temp/gauge24scaled.csv'
+	else:
+		file = 'data_temp/gauge24.csv'
+
+
+
+	df = pd.read_csv(file)
+
+	response = 'prec'
+
+	X = df.drop([response], axis=1)
+	y = df[response]
+
+	X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=seed)
+
+	return X_train, X_test, y_train, y_test
