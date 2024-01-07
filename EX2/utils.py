92f8a29d1e7856966f3a6250f27cf1bc084884e7
will amminger <willamminger@gmail.com>
2024-01-07 14:50:15 +0100

    update test table utils code

--- EX2/utils.py
+++ EX2/utils.py
@@ -3,25 +3,31 @@ import pandas as pd
 
 seed = 42
 
-def get_test_table(scaled=False):
-    if scaled:
-        file = 'data_temp/gauge24scaled.csv'
-    else:
-        file = 'data_temp/gauge24.csv'
 
-    df = pd.read_csv(file)
+def get_test_table(scaled: bool = False, train_size: int = .7):
+	"""
+	returns a train & test set from the temporary table located in /data_temp
+	:param scaled: whether to use the scaled or unscaled table
+	:param train_size: proportion of the data to use for training
+	:return: X_train, X_test, y_train, y_test
+	"""
+	if scaled:
+		file = 'data_temp/gauge24scaled.csv'
+	else:
+		file = 'data_temp/gauge24.csv'
 
-    df = df.sort_values('date')
+	df = pd.read_csv(file).drop('Unnamed: 0', axis=1)
+	df['date'] = pd.to_datetime(df['date'])
 
-    response = 'prec'
-    X = df.drop([response], axis=1)
-    y = df[response]
+	response = 'prec'
+	X = df.drop([response], axis=1)
+	y = df[response]
 
-    split_point = int(len(df) * 0.7)
-    
-    X_train = X.iloc[:split_point]
-    y_train = y.iloc[:split_point]
-    X_test = X.iloc[split_point:]
-    y_test = y.iloc[split_point:]
+	split_point = int(len(df) * train_size)
 
-    return X_train, X_test, y_train, y_test
+	return (
+		X.iloc[:split_point],
+		X.iloc[split_point:],
+		y.iloc[:split_point],
+		y.iloc[split_point:]
+	)
